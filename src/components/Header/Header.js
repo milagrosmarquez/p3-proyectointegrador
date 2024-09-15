089a4d61d299dfe4fb852e608f7bcd32d13e85f7
milagrosmarquez <mmarquezreymundo@udesa.edu.ar>
2024-09-15 15:47:47 -0300

    arreglo header

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,19 +1,17 @@
-import {Link} from "react-router-dom"
+import { Link } from "react-router-dom";
 
 const Header = () => {
-    return (
-      <header>
-        <div className="logo">
-          <h1>Metflix</h1> 
-        </div>
-        <nav>
-          <ul>
-            <li><Link to="/">Home</Link></li>
-            <li><Link to="/favoritos">Favoritos</Link></li>
-          </ul>
-        </nav>
-      </header>
-    );
-  };
-  
-  export default Header;
+  return (
+    <nav>
+      <ul className="logo">
+        <li><img src="./img/user.jpg" alt="Logo" /> </li>
+      </ul>
+      <ul>
+        <li><Link to="/">Home</Link></li>
+        <li><Link to="/favoritos">Favoritos</Link></li>
+      </ul>
+    </nav>
+  );
+};
+
+export default Header;
